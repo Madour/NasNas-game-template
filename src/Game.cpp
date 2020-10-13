9bb08a1133500e83cf92bfcb610bc484e47bf89f
Modar Nasser <modar1999@gmail.com>
2020-10-13 09:27:18 +0200

    update

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,17 +1,12 @@
 #include "Game.hpp"
 
 Game::Game() :ns::App("NasNas Game Template", 720, 480) {
+	ns::Config::debug = true;
 	addDebugText<std::string>([](){return "World !";}, "Hello", {0, 0});
 }
 
 void Game::onEvent(const sf::Event& event) {
-	if (event.type == sf::Event::Closed)
-		getWindow().close();
-
-	if (event.type == sf::Event::KeyReleased) {
-		if (event.key.code == sf::Keyboard::Escape)
-			getWindow().close();
-	}
+	ns::App::onEvent(event);
 }
 
 void Game::update() {
