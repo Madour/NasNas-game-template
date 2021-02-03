c0c1679147ea3d90f7d1c8bf191fdc7b48528c13
Modar Nasser <modar1999@gmail.com>
2021-02-03 19:12:48 +0100

    spaces

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,12 +1,12 @@
 #include "Game.hpp"
 
 Game::Game() :ns::App("NasNas Game Template", 720, 480) {
-	ns::Config::debug = true;
-	addDebugText("Hello World !", {20, 10});
+    ns::Config::debug = true;
+    addDebugText("Hello World !", {20, 10});
 }
 
 void Game::onEvent(const sf::Event& event) {
-	ns::App::onEvent(event);
+    ns::App::onEvent(event);
 }
 
 void Game::update() {
